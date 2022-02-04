b35c25735e805e944f6ea751279ea1b67bf6c42d
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-04 17:32:03 +0900

    initial commit

--- src/object/ObjectTest2.java
+++ src/object/ObjectTest2.java
@@ -1,5 +1,5 @@
 package object;
-/*클래스명 Emp1
+/*클래스명 Emp2
  * -name: String //멤버변수
  * -dept:String
  * -salary: int
@@ -7,30 +7,30 @@ package object;
  * +printEmp();void
  * */
 //ObjectTest1.java  ---> ObjectTest1.Class, Emp1.class
-class Emp1{
+class Emp2{
 	private String name;
 	private String dept;
 	private int salary;
 	
-	public void setEmp1(String name, String dept, int salary) {
+	public void setEmp2(String name, String dept, int salary) {
 		this.name=name;
 		this.dept=dept;
 		this.salary=salary;
 	}
 	
-	public void printEmp() {
-		System.out.println(name+"사원은 "+dept+"에 근무하며 "+salary+"원의 급여를 받습니다."); 
+	public String printEmp2() {
+		return name+"사원은 "+dept+"에 근무하며 "+salary+"원의 급여를 받습니다."; 
 	}
 }
-public class ObjectTest1 {
-	// 뽀로로 사원은 개발부에 근무하며 1520000원의 급여를 받습니다.
+public class ObjectTest2 {
+	// 크롱 사원은 개발부에 근무하며 1530000원의 급여를 받습니다.
 	
 	
 	
 	public static void main(String[] args) {
-		Emp1 ep=new Emp1();
-		ep.setEmp1("뽀로로","개발부",1520000);
-		ep.printEmp();
+		Emp2 ep=new Emp2();
+		ep.setEmp2("크롱","개발부",1530000);
+		System.out.println(ep.printEmp2());
 		
 	}
 
